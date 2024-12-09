7c08ad2dfdaf3f460d7b3ced4a9dd5cb63bae00f
Likitha reddy <gummadilikitha010@gmail.com>
2024-12-09 22:41:04 +0530

    main

---
+++ loops.js
@@ -0,0 +1,131 @@
+// for loop
+
+for(var a=1;a<5;a++){
+    console.log(a)
+}
+
+for(var a=17;a<25;a++){
+    console.log(a)
+}
+for(var b=30;b<=36;b++){
+    console.log(b)
+}
+for(var b=10;b>6;b--){
+    console.log(b)
+}
+for(var d=70;d>=66;d--){
+    console.log(d)
+}
+for(var a=17;a<3;a++){
+    console.log(a)
+}
+
+// while loop
+
+var a=10;
+while(a<16){
+    a++
+    console.log(a)
+}
+
+var b=20;
+while(b>10){
+    b--
+    console.log(b)
+   
+}
+var a=10;
+while(a>=6){
+   console.log(a)
+    a--
+}
+
+var a=80;
+while(a<=88){
+    a++
+    console.log(a)
+   
+}
+var s=50;
+while(s<=60){
+    console.log(s)
+    s++
+}
+
+var i=1
+while(i<4){
+    i++
+}
+console.log(i)
+
+// do while loop
+
+var i=1;
+do{
+    i++
+    console.log(i)
+}while(i<=6)
+
+var a=10
+do{
+    a--
+    console.log(a)
+}while(a>=5)
+
+var p=46;
+do{
+    console.log(p)
+    p++
+}while(p<=56)
+
+// 1. Printing Numbers from 1 to 10
+for(var i=1;i<=10;i++){
+    console.log(i);
+}
+
+// 2. Printing Even Numbers from 1 to 10
+for (let i = 2; i <= 10; i += 2) { 
+    console.log(i);
+  }
+
+for(let i=10;i>=2;i-=2){
+console.log(i)
+}
+  
+// 3. Printing Odd Numbers from 1 to 10
+for(var i=1;i<=10;i+=2){
+    console.log(i);
+}
+for(var i=9;i>=1;i-=2){
+    console.log(i);
+}
+
+// 1. Printing Numbers from 1 to 10 Using Post-Increment
+var a=1
+while(a<=10){
+    console.log(a);
+    a++;
+
+}
+var a=1
+while(a<=10){
+    a++;
+    console.log(a);
+   
+
+}
+
+// 2. Printing Numbers from 10 to 1
+var a=10;
+while(a>=1){
+    i--
+    console.log();
+   
+}
+
+var s=10;
+do {
+     console.log(s);
+    s--;
+} while(s>=1);
+
